14b170ebbbbd428347b06732d9938c3a661a5453
dangdang-01 <3180422969@qq.com>
2022-08-19 21:44:47 +0800

    fix: 构建修复

--- src/components.d.ts
+++ src/components.d.ts
@@ -50,6 +50,7 @@ declare module '@vue/runtime-core' {
     IEpHistogram: typeof import('~icons/ep/histogram')['default']
     IEpHomeFilled: typeof import('~icons/ep/home-filled')['default']
     IEpPlus: typeof import('~icons/ep/plus')['default']
+    IEpPosition: typeof import('~icons/ep/position')['default']
     IEpRefresh: typeof import('~icons/ep/refresh')['default']
     IEpSearch: typeof import('~icons/ep/search')['default']
     IEpSetting: typeof import('~icons/ep/setting')['default']
