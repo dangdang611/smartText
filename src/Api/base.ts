857430df67db56f8ea8f3bf73fd03a155fac174e
dangdang-01 <3180422969@qq.com>
2022-08-21 19:23:32 +0800

    fix: 部署服务器接口

--- src/Api/base.ts
+++ src/Api/base.ts
@@ -2,6 +2,6 @@ export const base = {
   server1: "https://demo-api.apipost.cn",
   server2: "http://43.138.36.13:9526",
   server4: "http://101.35.194.184:8080",
-  server3: "http://127.0.0.1:7001/api",
+  server3: "http://39.99.80.36:7001/api",
   AIroot: "http://43.138.36.13:16668",
 };
